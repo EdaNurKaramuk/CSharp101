f4c73e46b5c2b1a70e205f363cc22f5e54ca89e4
Eda Nur Karamuk <edanurkaramuk@gmail.com>
2021-06-26 17:40:41 +0300

    C# 101 Console Programlama

---
+++ Pratikler/console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " " + surname);
+        }
+    }
+}
