ff70900dba42fe27040e8443a4832a898c74059b
Eda Nur Karamuk <edanurkaramuk@gmail.com>
2021-07-18 00:52:11 +0300

    C# 101 OOP - OOP ve Prensipleri Nedir?

---
+++ Pratikler/kalitim-inheritance/Hayvanlar.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace kalitim_inheritance
+{
+    public class Hayvanlar : Canlilar
+    {
+        public void SuruHalindeHareket()
+        {
+            Console.WriteLine("Hayvanlar sürüler halinde hareket eder.");
+        }
+    }
+
+    public class Kus : Hayvanlar
+    {
+        public void Ucmak()
+        {
+            Console.WriteLine("Kuşlar uçar");
+        }
+    }
+
+    public class Ordek : Hayvanlar
+    {
+        public void Yuzmek()
+        {
+            Console.WriteLine("Ördekler yüzer");
+        }
+    }
+
+}
---
+++ Pratikler/kalitim-inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace kalitim_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TohumluBitkiler tohumluBitki = new TohumluBitkiler();
+            tohumluBitki.Beslenme();
+            tohumluBitki.Bosaltim();
+            tohumluBitki.Solunum();
+            tohumluBitki.FotosentezYapmak();
+            tohumluBitki.TohumlaCogalma();
+
+            Console.WriteLine("**********************");
+
+            Ordek ordek = new Ordek();
+            ordek.Solunum();
+            ordek.Beslenme();
+            ordek.Bosaltim();
+            ordek.SuruHalindeHareket();
+            ordek.Yuzmek();
+        }
+    }
+}
