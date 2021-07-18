e61a96dcef3165483fb16b3f425956c43213b027
Eda Nur Karamuk <edanurkaramuk@gmail.com>
2021-07-18 14:40:01 +0300

    C# 101 OOP - Interface(Arayüz) Örnek

---
+++ Pratikler/arayuzler-interface-ornek/Civic.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace arayuzler_interface_ornek
+{
+    public class Civic : IOtomobil
+    {
+        public Marka HangiMarkaninAraci()
+        {
+            return Marka.Honda;
+        }
+
+        public int KacTekerlektenOlusur()
+        {
+            return 4;
+        }
+
+        public Renk StandartRengiNe()
+        {
+            return Renk.Gri;
+        }
+    }
+}
---
+++ Pratikler/arayuzler-interface-ornek/Sabitler.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace arayuzler_interface_ornek
+{
+    public enum Marka
+    {
+        Ford,
+        Toyota,
+        Honda
+    }
+
+    public enum Renk
+    {
+        Beyaz,
+        Gri
+    }
+}
